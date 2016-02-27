97f473fa72bd757977cdd36a429e4c89402f72fe
devilmustcry <eaglization@gmail.com>
2016-02-27 18:48:44 +0700

    Create layout and link them together

---
+++ MiniTrello/app/src/main/java/com/sandstorm/softspec/mini_trello/activities/CardActivity.java
@@ -0,0 +1,15 @@
+package com.sandstorm.softspec.mini_trello.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sandstorm.softspec.mini_trello.R;
+
+public class CardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+    }
+}
