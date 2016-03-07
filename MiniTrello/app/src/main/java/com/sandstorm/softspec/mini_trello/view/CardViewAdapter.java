23ab3e3b6cce7f239d7a23d53a2449c1a05c8019
devilmustcry <eaglization@gmail.com>
2016-03-07 17:58:41 +0700

    In progress in change list page into card view

--- MiniTrello/app/src/main/java/com/sandstorm/softspec/mini_trello/view/CardViewAdapter.java
+++ MiniTrello/app/src/main/java/com/sandstorm/softspec/mini_trello/view/CardViewAdapter.java
@@ -1,8 +1,23 @@
 package com.sandstorm.softspec.mini_trello.view;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by FTTX on 3/7/2016 AD.
  */
 public class CardViewAdapter {
+
+    public static class CardViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+        TextView date;
+
+        public CardViewHolder(View itemView) {
+            super(itemView);
+
+        }
+    }
 
 }
