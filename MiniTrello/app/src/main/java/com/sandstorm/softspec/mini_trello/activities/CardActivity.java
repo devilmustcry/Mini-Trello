70e75f1a258ba8c971ac987668638774e5dfa87d
devilmustcry <eaglization@gmail.com>
2016-03-09 20:39:22 +0700

    Create comment dialog (still cannot create comment)

--- MiniTrello/app/src/main/java/com/sandstorm/softspec/mini_trello/activities/CardActivity.java
+++ MiniTrello/app/src/main/java/com/sandstorm/softspec/mini_trello/activities/CardActivity.java
@@ -1,15 +1,21 @@
 package com.sandstorm.softspec.mini_trello.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sandstorm.softspec.mini_trello.R;
 import com.sandstorm.softspec.mini_trello.models.Card;
 import com.sandstorm.softspec.mini_trello.models.CardList;
 import com.sandstorm.softspec.mini_trello.models.Storage;
 
-public class CardActivity extends AppCompatActivity {
+public class CardActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Card card;
 //    private Card dummyCard;
@@ -17,6 +23,37 @@ public class CardActivity extends AppCompatActivity {
     private CardList dummyList;
     private TextView title;
     private TextView description;
+    private AlertDialog.Builder dialogBuilder;
+    private Button commentButton;
+
+
+    private void commentDialog() {
+        dialogBuilder = new AlertDialog.Builder(this);
+        EditText nameInput = new EditText(this);
+        EditText commentInput = new EditText(this);
+        dialogBuilder.setTitle("Comment");
+        dialogBuilder.setMessage("Your Name?");
+        dialogBuilder.setView(nameInput);
+        dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+
+        });
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog commentDialog = dialogBuilder.create();
+        commentDialog.show();
+
+    }
 
 
     @Override
@@ -39,6 +76,18 @@ public class CardActivity extends AppCompatActivity {
 
         title.setText(card.getTitle());
         description.setText(card.getDescription());
+
+        commentButton = (Button) findViewById(R.id.comment_card_button);
+        commentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                commentDialog();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
